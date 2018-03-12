bdebafe89aa490d33bb43ab73776beb407695cc2
Rami <rami_gomaa@web.de>
2018-03-12 19:54:04 +0100

    Changed Navigation

--- static/js/animation.js
+++ static/js/animation.js
@@ -4,14 +4,13 @@ window.addEventListener("load",function(){
     let headerNavMainTriggerd = document.querySelector(".header--nav--main>ul");
     headerNavMain.addEventListener("click",function(){
         headerNavMainTriggerd.classList.toggle("triggered");
-
     },false);
     //Navigation right
     let headerNavProfil = document.querySelector(".header--nav--profil");
     let headerNavProfilTriggerd = document.querySelector(".header--nav--profil>ul");
+
     headerNavProfil.addEventListener("click",function(){
         headerNavProfilTriggerd.classList.toggle("triggered");
-
     },false);
 
 },false);
