2cdefde1433795405556702ef6ece5df3b4a8ec9
Bennet Russnak <33715386+Bemmell@users.noreply.github.com>
2018-04-12 19:41:54 +0200

    Register Form Designed

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/login', (req, res) => {
     res.render('login')
     return true
 })
+router.get('/register', (req, res) => {
+    res.render('register')
+    return true
+})
 
 router.get('/', (req, res) => {
     res.render('index')
