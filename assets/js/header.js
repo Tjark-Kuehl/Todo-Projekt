5ec40ce319b699db852871fcbc93e5ce02aa3f72
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-05-15 21:42:16 +0200

    Rebuild header. Moved logout to top bar

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,13 +7,4 @@ document.addEventListener('DOMContentLoaded', () => {
                 .querySelector('.header--nav--main>ul')
                 .classList.toggle('triggered')
         })
-
-    //Navigation right
-    document
-        .querySelector('.header--nav--profil')
-        .addEventListener('click', () => {
-            document
-                .querySelector('.header--nav--profil>ul')
-                .classList.toggle('triggered')
-        })
 })
